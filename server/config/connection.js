c3705590b4b096acde6442bdd6a1a4792d078160
ajrego13 <ajrego13@gmail.com>
2021-12-01 19:22:13 -0500

    fixed backend

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/symphony',
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
   }
 );
 
